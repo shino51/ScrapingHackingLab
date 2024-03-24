22dfb40bb991ebcd69be030af599252fa34ec39a
shino51 <shino.takeshita1124@gmail.com>
2024-03-24 20:22:52 +0100

    Added scrapy code to crawl hatelabo entries

--- anond/anond/items.py
+++ anond/anond/items.py
@@ -9,4 +9,4 @@ import scrapy
 class AnondItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
---
+++ anond/anond/spiders/anond_spider.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class AnondSpiderSpider(scrapy.Spider):
+    name = "anond_spider"
+    allowed_domains = ["anond.hatelabo.jp"]
+    start_urls = ["https://anond.hatelabo.jp"]
+
+    def parse(self, response, **kwargs):
+        for url in response.css('p.sectionfooter a::attr("href")'):
+            yield response.follow(url)
